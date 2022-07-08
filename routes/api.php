74457168d5bb5de0f4606c32181797204d1f5e9d
G <g@g.com>
2022-07-07 22:38:16 -0300

    second

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('dacxi', 'ApiDacxiController');
+
+Route::get('status', 'ApiDacxiController@statusGecko');
